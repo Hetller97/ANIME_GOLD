ef79ec0701cde9d46444428c3703ec20f26ef4c3
Hetller97 <hettler9797@gmail.com>
2025-01-26 19:37:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("goTOregions").addEventListener("click", function() {
+    window.location.href = "regions.html";
+});
+
+document.getElementById("goTOkitaat").addEventListener("click", function() {
+    window.location.href = "kitaat.html";
+}); 
+
+document.getElementById("goTOmanagement").addEventListener("click", function() {
+    window.location.href = "management.html";
+}); 
+
+document.getElementById("regionsPage").addEventListener("click", function() {
+    window.location.href = "faraina.html";
+});
+
